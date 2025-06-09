e6707ab38bae428414b039333cbcf802b28ad303
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-06-09 17:32:07 +0530

    More bun && million

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-// filepath: /Users/nadhi/Documents/GitHub/Final-Laravel-react frameowkr /Kushi-Dash/vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 import MillionLint from "@million/lint";
+import million from 'million/compiler';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.jsx'],
-            ssr: 'resources/js/ssr.jsx', // Add this line
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
+        }),
+        million.vite({
+            auto: true, // Enable automatic mode for component optimization
+            mute: false, // Set to true to reduce logs
         }),
         react(),
         MillionLint.vite()
