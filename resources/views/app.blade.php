789ebbcd45a9d9f68b9b57a40e66fdd36a99b174
KushiKreates <136042271+KushiKreates@users.noreply.github.com>
2025-06-11 08:36:01 +0530

    Ssr updates and removing some stuff

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -24,21 +24,46 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?fam
-        ily=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         
         <!-- Fonts -->
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Doto:wght@100..900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Doto:wght@100..900&display=swap" rel="stylesheet">
+
+        <!-- Kushi Data Script - Make all Inertia data available globally -->
+        <script>
+            window.KushiData = @json(\Inertia\Inertia::getShared());
+            window.KushiData.timestamp = new Date().toISOString();
+            
+            // Easy access functions
+            window.getAuth = () => window.KushiData.auth;
+            window.getUser = () => window.KushiData.auth?.user;
+            window.getShop = () => window.KushiData.shop;
+            window.getResources = () => window.KushiData.totalResources;
+            window.getDebug = () => window.KushiData.debug;
+            window.getFlash = () => window.KushiData.flash;
+            window.getVmsConfig = () => window.KushiData.vmsConfig;
+            
+            @if(app()->environment('local', 'staging'))
+            // Console log for development
+            console.group('🚀 Kushi Data Available');
+            console.log('Full Data:', window.KushiData);
+            console.log('Auth:', window.getAuth());
+            console.log('User:', window.getUser());
+            console.log('Resources:', window.getResources());
+            console.log('Debug:', window.getDebug());
+            console.groupEnd();
+            @endif
+        </script>
 
         <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
-        @inertiaHead
+        
     </head>
     <body class="font-sans antialiased">
         <!-- Interia App with react render -->
